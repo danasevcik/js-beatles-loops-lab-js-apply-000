eecb5e7ad0f97c17585774bf7402859aa6a55ad1
Dana Sevcik <danasevcik@hotmail.com>
2018-11-06 01:14:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function theBeatlesPlay(musArr, instArr) {
   for (var i = 0; i < musArr.length; i++) {
     var musEle = musArr[i];
     var instEle = instArr[i];
-    var musInstStr = musEle + ' plays ' + 
+    var musInstStr = musEle + ' plays ' + instEle;
+    
+    
   }
 }
